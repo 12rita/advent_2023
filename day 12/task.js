71a8f77371c860f9b5dd04eca1f7f4dadb226659
Margo_Phuket_Office <m.goncharova@group-ib.com>
2023-12-15 00:22:24 +0700

    Day 12 has been done!!

--- day 12/task.js
+++ day 12/task.js
@@ -1,9 +1,25 @@
 import {dataInput, testInput} from "./input.js";
 
-const example = `????????##?.#. 1,8,1`;
 
-const data = testInput.split('\n');
+const data = dataInput.split('\n');
 // ..#.#..###...##. 1,1,3
+const cache = {};
+
+const memoize = (
+    func
+) => {
+    const stored = {};
+
+    return (...args) => {
+        const k = JSON.stringify(args);
+        if (stored[k]) {
+            return stored[k];
+        }
+        const result = func(...args);
+        stored[k] = result;
+        return result;
+    };
+}
 const findArrangementsCount = (input) => {
     const [pattern, groups] = input.split(' ');
 
@@ -24,8 +40,8 @@ const findArrangementsCount = (input) => {
     const dotted = pattern.substring(0, firstQM) + '.' + pattern.substring(firstQM + 1);
     return findArrangementsCount(dashed + ' ' + groups) + findArrangementsCount(dotted + ' ' + groups);
 
-
 }
+
 
 const checkValidBeforeQM = (pattern, groups) => {
     const numberGroups = groups.split(',').map((el) => Number(el));
@@ -56,72 +72,72 @@ const checkValidBeforeQM = (pattern, groups) => {
 
 }
 
-const optimizedArrangementsCount = ({cache, pattern, patternIdx, group, groupIdx, currentGroup}) => {
-    const key = `${patternIdx},${groupIdx},${currentGroup}`;
-    if (key in cache) return cache[key];
-    if (patternIdx === pattern.length) {
-        if (groupIdx === group.length - 1 && currentGroup === 0) {
+const optimizedArrangementsCount = memoize((pattern, groups) => {
+    // const key = `${patternIdx},${groupIdx},${currentGroup}`;
+    // const [pattern, groups] = line.split(' ');
+    // console.log({pattern, groups});
+    if (cache[pattern + groups.join('')]) {
+        return cache[pattern + groups.join('')];
+    }
+    const numberGroups = groups;
+
+    if (pattern.length === 0) {
+        if (numberGroups.length === 0) {
             return 1;
         }
-        if (groupIdx === group.length && currentGroup === group[groupIdx]) {
-            return 1;
-        } else return 0;
+        return 0;
     }
-    let arrangementsCount = 0;
-    if (pattern[patternIdx] === '#' || pattern[patternIdx] === '?') {
-        arrangementsCount += optimizedArrangementsCount({
-            cache,
-            pattern,
-            patternIdx: patternIdx + 1,
-            group,
-            groupIdx,
-            currentGroup: currentGroup + 1
-        })
-        if (pattern[patternIdx] === '.' || pattern[patternIdx] === '?') {
-            if (currentGroup === 0) {
-                arrangementsCount += optimizedArrangementsCount({
-                    cache,
-                    pattern,
-                    patternIdx: patternIdx + 1,
-                    group,
-                    groupIdx,
-                    currentGroup: 0
-                })
-            } else if (currentGroup > 0 && currentGroup === group[groupIdx] && groupIdx < group.length) {
-                arrangementsCount += optimizedArrangementsCount({
-                    cache,
-                    pattern,
-                    patternIdx: patternIdx + 1,
-                    group,
-                    groupIdx: groupIdx + 1,
-                    currentGroup: 0
-                })
+    if (numberGroups.length === 0) {
+        for (let i = 0; i < pattern.length; i++) {
+            if (pattern[i] === "#") {
+                return 0;
             }
+        }
+        return 1;
+    }
 
+    const sum = numberGroups.reduce((acc, el) => acc + el, 0);
+
+    if (pattern.length < sum + numberGroups.length - 1) {
+        // The line is not long enough for all runs
+        return 0;
+    }
+
+    if (pattern[0] === ".") {
+        return cache[pattern.slice(1) + [...numberGroups].join('')] = optimizedArrangementsCount(pattern.slice(1), [...numberGroups]);
+    }
+    if (pattern[0] === "#") {
+        const currGroup = numberGroups.shift();
+        for (let i = 0; i < currGroup; i++) {
+            if (pattern[i] === ".") {
+                return 0;
+            }
+        }
+        if (pattern[currGroup] === "#") {
+            return 0;
         }
 
+        return cache[pattern.slice(currGroup + 1) + [...numberGroups].join('')] = optimizedArrangementsCount(pattern.slice(currGroup + 1), [...numberGroups]);
     }
-    return (cache[key] = arrangementsCount);
 
-}
+    return (
+        (cache["#" + pattern.slice(1) + [...numberGroups].join('')] = optimizedArrangementsCount("#" + pattern.slice(1), [...numberGroups])) + (cache["." + pattern.slice(1) + [...numberGroups].join('')] = optimizedArrangementsCount("." + pattern.slice(1), [...numberGroups]))
+    );
 
-const result1 = data.reduce((acc, el) => {
+})
+
+const result1 = data.reduce((acc, el, i) => {
     // console.log(el,findArrangementsCount(el))
     const [pattern, groups] = el.split(' ');
     const newPattern = pattern + '?' + pattern + '?' + pattern + '?' + pattern + '?' + pattern;
     const newGroups = (groups + ',' + groups + ',' + groups + ',' + groups + ',' + groups).split(',').map((el) => Number(el));
-    const arrangementsCount = optimizedArrangementsCount({
-        cache: {},
-        pattern: newPattern,
-        patternIdx: 0,
-        group: newGroups,
-        groupIdx: 0,
-        currentGroup: 0
-    })
-    // console.log({pattern, groups, arrangementsCount})
+    // console.log({newPattern, newGroups})
+    const arrangementsCount = optimizedArrangementsCount(newPattern, newGroups)
+    console.log({arrangementsCount, i})
     return acc + arrangementsCount
 }, 0)
 
 //11870 too high
 //7718 correct
+//128741994134728
 console.log(result1)
